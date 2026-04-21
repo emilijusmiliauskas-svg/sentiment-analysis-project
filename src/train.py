3ca70e55d24647b8b41bb5ef5f6f87f0d49a1fc6
emil <emilijusmiliauskas@gmail.com>
2026-04-21 17:08:02 +0200

    Revert "docs: Update README with new function info"
    This reverts commit c0a0e069318dd8130739c090fccda664c2003ff6.

--- src/train.py
+++ src/train.py
@@ -75,4 +75,3 @@ if __name__ == "__main__":
 
     args: argparse.Namespace = parser.parse_args()
     main(data_path=args.data, model_path=args.out)
-# A comment that was forgotten in the previous commit.
